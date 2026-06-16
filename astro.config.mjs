9c16319137d29986eb0431df79882fcb831ff66c
Shoji-Kikuchi7580 <picbig25@gmail.com>
2026-06-17 01:55:07 +0900

    静的出力に切り替え(Cloudflare Pages対応)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
-import netlify from '@astrojs/netlify';
 import react from '@astrojs/react';
 import keystatic from '@keystatic/astro';
 import { SITE } from './src/config';
 
+const isDev = process.argv.includes('dev');
+
 export default defineConfig({
   site: SITE.url,
-  adapter: netlify(),
-  integrations: [sitemap(), react(), keystatic()],
+  integrations: [sitemap(), react(), ...(isDev ? [keystatic()] : [])],
 });
